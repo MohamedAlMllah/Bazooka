59a7c6e7be320f79fab4d9b58e29f2fa65a6b717
mohamedalmllah <you@example.com>
2022-11-29 15:15:41 +0200

    super admin role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->unsignedBigInteger('role_id')->default('2');
+            $table->unsignedBigInteger('role_id')->default('5');
             $table->foreign('role_id')->references('id')->on('roles');
             $table->string('password');
             $table->rememberToken();
